15f2f3215105e330edd260beb3d12fef3682ce4d
AG89Z <agiachino@zmorph3d.com>
2020-05-20 11:35:12 +0200

    Setup router

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,42 @@
-import * as React from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
 
-function App(){
+import LoginPage from './pages/login';
+import DashboardPage from './pages/dashboard';
+
+export default function App() {
   return (
-    <h1>Hi there</h1>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/login"/>
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/dashboard">
+            <DashboardPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
