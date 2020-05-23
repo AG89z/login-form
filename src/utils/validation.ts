12d4b48403d11f601b1f3ea66d5af5d6dc719a4d
AG89Z <agiachino@zmorph3d.com>
2020-05-23 19:38:19 +0200

    More function validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -3,22 +3,67 @@ export interface ValidationError {
   message: string;
 }
 
-function nonEmpty(email: string) {
-  if (email.length > 0) {
-    return true;
-  }
-  return false;
+interface Validator {
+  (value: string | ValidationError): string | ValidationError;
 }
 
-export function validateEmail(email: string): true | ValidationError {
-  if (!nonEmpty(email)) {
-    return {
-      error: 'EMPTY_EMAIL_STRING',
-      message: 'The email field cannot be empty',
-    };
+function isError(value: string | ValidationError): value is ValidationError {
+  return (value as ValidationError).error !== undefined;
+}
+
+const makeValidator = (
+  validateFn: (email: string) => boolean,
+  error: ValidationError
+): Validator => (value) => {
+  if (isError(value)) {
+    return value;
   }
 
-  return true;
+  if (validateFn(value)) {
+    return value;
+  }
+
+  return error;
+};
+
+const nonEmpty = makeValidator(
+  (email) => {
+    if (email.length > 0) {
+      return true;
+    }
+    return false;
+  },
+  {
+    error: 'EMPTY_EMAIL_STRING',
+    message: 'The email field cannot be empty',
+  }
+);
+
+const notTooLong = makeValidator(
+  (email) => {
+    if (email.length < 10) {
+      return true;
+    }
+    return false;
+  },
+  {
+    error: 'EMAIL_TOO_LONG',
+    message: 'The email is too long',
+  }
+);
+
+const composeValidators = (...validators: Validator[]) => (
+  value: string | ValidationError
+) => validators.reduce((res, validate) => validate(res), value);
+
+export function validateEmail(email: string): true | ValidationError {
+  const validEmail = composeValidators(nonEmpty, notTooLong)(email);
+
+  if (!isError(validEmail)) {
+    return true;
+  }
+
+  return validEmail;
 }
 
 export function validatePassword(password: string): true | ValidationError {
