471f97d75fa2ff0b6d61fe1bed2b0bf0168aa0a0
AG89Z <agiachino@zmorph3d.com>
2020-05-24 12:41:42 +0200

    Add snapshot test

---
+++ tests/loginform.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import renderer from 'react-test-renderer';
+import { LoginForm } from '../src/components/LoginForm';
+
+it('renders correctly', () => {
+  const tree = renderer.create(<LoginForm />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
